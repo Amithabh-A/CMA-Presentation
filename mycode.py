34c2580d5e4ef2296ae60b322a8a91d3c07512c1
Amithabh-A <amithabh2002@gmail.com>
2024-03-31 17:02:47 +0530

    changing mycode.py's nextPoint fn to the one implemented by oscar veliz.

--- mycode.py
+++ mycode.py
@@ -3,30 +3,25 @@ from Polynomial import BestFitPolyFunction
 import cmath
 
 def FirstOrderDividedDifference(x0, x1, f):
-    print(x1-x0)
     return (f(x1) - f(x0)) / (x1 - x0)
 
 def SecondOrderDividedDifference(x0, x1, x2 ,f):
     return (FirstOrderDividedDifference(x1,x2,f) - FirstOrderDividedDifference(x0,x1,f)) / (x2-x0)
 
 def FindRoot(p, x0,x1,x2,ε):
-#    print("inside findroot")
     print(abs(p(x2)), ε )
-    j  = 0
+#    j  = 0
+    i = 4
     while(abs(p(x2)) > ε):
-        j += 1
-        if(j > 10) : break
-#        print("inside while loop")
+#        j += 1
+#        if(j > 10) : break
         next_x = NextPoint(x0,x1,x2,p)
         print(next_x)
-        i = 4
         Show(i, next_x, p)
-#        x0 = x1
-#        x1 = x2
-#        x2 = next_x
-        x0 = next_x
-        x1 = x0
-        x2 = x1
+        x0 = x1
+        x1 = x2
+        x2 = next_x
+        i += 1
 
 def Show(i, x, f):
     if x.imag == 0j:
@@ -40,20 +35,23 @@ def W(x0,x1,x2,f):
     return FirstOrderDividedDifference(x2,x1,f) + FirstOrderDividedDifference(x2,x0,f) - FirstOrderDividedDifference(x0,x1,f)
 
 # alternative for calculate_next_x
+
 def NextPoint(x0,x1,x2,f):
-    return x2 - (2 * f(x2)) / (W(x0,x1,x2,f) + cmath.sqrt(W(x0,x1,x2,f)**2 - 4 * f(x2) * SecondOrderDividedDifference(x2,x1,x0,f)))
+    # Why the normal way does not works? 
+    q = (x0 - x1) / (x1 - x2) # f [xnm1, xn]
+    a = q * f(x0) - q * (1 + q) * f(x1) + q**2 * f(x2)
+    b = (2 * q + 1) * f(x0) - (1 + q)**2 * f(x1) + q**2 * f(x2)
+    c = (1 + q) * f(x0)
+    return x0 - (x0 - x1) * ((2 * c) / (b + cmath.sqrt(b**2 - 4 * a * c)))
+
+#    return x2 - (2 * f(x2)) / (W(x0,x1,x2,f) + cmath.sqrt(W(x0,x1,x2,f)**2 - 4 * f(x2) * SecondOrderDividedDifference(x2,x1,x0,f)))
 
 if __name__ == "__main__":
     # testcases
 
     #l = [int(x) for x in input("Enter elements separated by space: ").split()]
-#    p = Polynomial([-1, -4, 0, 1])
     p = Polynomial([-1, -1, -1, 1])
-    print("before myroot()")
-    FindRoot(p,0,1,2,1e-7)
-    print("after myroot()")
-#    x0 = 10
-#    x1 = 9
-#    x2 = 8
-#    x3 = NextPoint(x0,x1,x2,p)
-#    print(x3)
+#    print(NextPoint(2,1,0,p))
+#    print("before myroot()")
+    FindRoot(p,2,1,0,1e-7)
+#    print("after myroot()")
